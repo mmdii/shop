537e144392757f9f461a658b5bd28306f3cd4d02
mohammad mohammadi <mohammad.mohammadii1997@gmail.com>
2019-03-19 15:34:57 +0330

    use handlebars

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine('handlebars',expressHbs());
+app.set('view engine', 'handlebars');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,'public')));
